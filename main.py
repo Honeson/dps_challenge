486c8f58f94f3f0bb0bf1ca4a965ba9ec9757ef0
Sunday Nwovu <sundaynwovuhonesty@gmail.com>
2022-06-17 20:49:27 +0100

    FastAPI completed

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import joblib
 from pydantic import BaseModel
 import uvicorn
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 
 app = FastAPI()
 
 class DataInput(BaseModel):
-    JAHR: int
-    Month: int
     MONATSZAHL: str
     AUSPRAEGUNG: str
+    JAHR: int
+    Month: int
 
 
 def load_model():
@@ -22,30 +23,28 @@ def load_model():
 def prepare_new_data(df):
     vars_cat = [col for col in df.columns if df[col].dtypes=='O']
     #vars_cat = ['MONATSZAHL', 'AUSPRAEGUNG']
-
+    labelencoder = LabelEncoder()
     for var in vars_cat:
-        #df = pd.concat([df,pd.get_dummies(df[var], prefix=var, drop_first=True)], axis=1)
-        #df = pd.get_dummies(df[var], prefix=var, drop_first=True)
-        #df[var] = pd.get_dummies(df[var])
-        df = pd.concat([df,pd.get_dummies(df[var], prefix=var, drop_first=False)], axis=1)
     
-    df.drop(labels=vars_cat, axis=1, inplace=True)
+        df[var] = labelencoder.fit_transform(df[var])
+    
     print(df.describe())
     print(df.head())
     return df
 
 @app.post('/predict')
-def predict(data: DataInput):
+async def predict(data: DataInput):
     input_data = []
-    JAHR = data.JAHR
-    Month = data.Month
     MONATSZAHL = data.MONATSZAHL
     AUSPRAEGUNG = data.AUSPRAEGUNG
-    input_data.append([JAHR, Month, MONATSZAHL, AUSPRAEGUNG])
-    df = pd.DataFrame(input_data, columns=['JAHR', 'Month', 'MONATSZAHL', 'AUSPRAEGUNG'])
+    JAHR = data.JAHR
+    Month = data.Month
+    input_data.append([ MONATSZAHL, AUSPRAEGUNG, JAHR, Month])
+    df = pd.DataFrame(input_data, columns=['MONATSZAHL', 'AUSPRAEGUNG', 'JAHR', 'Month'])
+    print(df)
     model = load_model()
     value = model.predict(prepare_new_data(df))
-    return {'Value': value}
+    return {'Value': value[0]}
 
 
 
